ef178be8a223dc94bed35832640bdd28c0c43ace
Gabryel <gabryel.macedo@gmail.com>
2025-03-17 13:00:18 -0300

    challenge amigo secreto

---
+++ app.js
@@ -0,0 +1,37 @@
+let listaDeAmigos = [];
+
+function adicionarAmigo(){
+    const adicionarNome = document.getElementById('amigo')
+    const nomeAdicionado = adicionarNome.value.trim() 
+
+    if(nomeAdicionado === ''){
+        alert('Campo vazio! Por favor, insira um nome.')
+        return
+    }
+
+    listaDeAmigos.push(nomeAdicionado)
+
+    atualizarLista()
+    adicionarNome.value = ''
+}
+
+function atualizarLista(){
+    const listaAmigos = document.getElementById('listaAmigos')
+    listaAmigos.innerHTML = ''
+    
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        listaAmigos.innerHTML += "<li>" + listaDeAmigos[i] + "</li>";
+    }
+}
+
+function sortearAmigo(){
+    if(listaDeAmigos.lenth <= 1){
+        alert("Insira pelo menos dois nomes antes de sortear.")
+        return
+    }
+
+    let posicaoSorteada = Math.floor(Math.random() * listaDeAmigos.length)
+    let amigoSorteado = listaDeAmigos[posicaoSorteada]
+
+    document.getElementById('resultado').innerHTML = '<li>Amigo Sorteado: ' + amigoSorteado + '</li>'
+}
